91739acfca91281879a47aaf28fbe4d24c233b8b
dancingrooz <dancingrooz@gmail.com>
2020-01-22 01:33:01 +0100

    Add files via upload
    Hierbij mijn code voor Frontend Development

---
+++ Frontend Opdracht/web.js
@@ -0,0 +1,7 @@
+function toggleHamburger() {
+    if (document.querySelector("nav ul").style.display != 'flex') {
+        document.querySelector("nav ul").style.display = 'flex';
+    } else {
+        document.querySelector("nav ul").style.display = 'none';
+    }
+}
